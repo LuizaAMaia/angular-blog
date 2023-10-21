d648d8c1ad74c9f960818639d278d258818159bb
LuizaAMaia <araujomluiza@gmail.com>
2023-10-21 18:40:55 -0300

    content created

---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Novas atualizações do Angular",
+    "description": "Novas atualizações do Angular conta com bug fixes",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSknTsWM2nkoZ8PlwTkGRQqN2Zcjag4fj7RA&usqp=CAU"
+  },
+  {
+    "id": "2",
+    "title": "Fernanda Paes Leme avisa 'Fiz o número 2'",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel justo diam. Praesent ut suscipit nisi. Nam scelerisque sodales luctus.Suspendisse vitae nulla non lorem laoreet eleifend. Aliquam blandit justo ut orci porta euismod. Proin elementum faucibus leo non aliquam. Fusce nec blandit sapien, vel luctus dolor. Nam nunc dolor, commodo vel justo at, pulvinar mattis libero. Mauris nec nisl non risus porta efficitur. Nullam eu pellentesque turpis, quis fermentum enim. Phasellus ac enim nec nulla congue rhoncus.Nulla ut augue a purus vulputate posuere. Suspendisse imperdiet vehicula sem non interdum. Mauris dui metus, ornare sit amet consequat in, luctus sit amet risus. Aenean turpis risus, eleifend congue augue volutpat, cursus pharetra tellus. Pellentesque placerat diam at dui tempus, eu vehicula justo consectetur. Etiam luctus lorem in consequat malesuada. Pellentesque dignissim sapien in sapien elementum pulvinar. Pellentesque vulputate lacus aliquam nisl aliquet, non facilisis massa lobortis.",
+    "photoCover": "https://res.cloudinary.com/buzzfeed-brasil/image/upload/q_auto,f_auto/image-uploads/subbuzz-images/jpeg/02652acf8b01cbe3708c3d0a758c4e09.jpg"
+  }
+]
